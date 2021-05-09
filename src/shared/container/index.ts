bcc410542fefbed26253f97aa076823b013fb60d
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-05-09 20:16:20 -0300

    :hammer: add exams and laboratories repositories to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,19 @@
+import { container } from 'tsyringe';
+
 import './providers';
+
+import ILaboratoriesRepository from '@modules/laboratories/repositories/ILaboratoriesRepository';
+import TypeORMLaboratoriesRepository from '@modules/laboratories/repositories/implementations/TypeORMLaboratoriesRepository';
+
+import IExamsRepository from '@modules/exams/repositories/IExamsRepository';
+import TypeORMExamsRepository from '@modules/exams/repositories/implementations/TypeORMExamsRepository';
+
+container.registerSingleton<ILaboratoriesRepository>(
+  'LaboratoriesRepository',
+  TypeORMLaboratoriesRepository,
+);
+
+container.registerSingleton<IExamsRepository>(
+  'ExamsRepository',
+  TypeORMExamsRepository,
+);
