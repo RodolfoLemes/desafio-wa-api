d658236447bd98753c8b05f91ff24b05db9ac230
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-05-12 21:05:54 -0300

    :pencil2: type: remove sortingOptions on express types

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,11 +1,9 @@
 import IPaginationOptions from '@modules/pagination/interfaces/IPaginationOptions';
-import ISortingOptions from '@modules/sorting/interfaces/ISortingOptions';
 
 declare global {
   namespace Express {
     interface Request {
       paginationOptions: IPaginationOptions;
-      sortingOptions: ISortingOptions;
     }
   }
 }
